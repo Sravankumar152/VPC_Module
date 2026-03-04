dd1c10766500c924a5c6d7ab977e51f957b87b7f
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-03-04 14:14:11 +0530

    fixed vpc peering connection id issue

--- peering.tf
+++ peering.tf
@@ -27,26 +27,24 @@ resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = var.public_cidr_dest
-  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+  vpc_peering_connection_id  = aws_vpc_peering_connection.main.id
 }
 
 resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
-  vpc_peering_connection_id  = aws_vpc_peering_connection.main[count.index].id
+  vpc_peering_connection_id  = aws_vpc_peering_connection.main.id
 }
 
 resource "aws_route" "database_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
-  vpc_peering_connection_id  = aws_vpc_peering_connection.main[count.index].id
-}
+  vpc_peering_connection_id  = aws_vpc_peering_connection.main.id
 
 resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.default.id
   destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id  = aws_vpc_peering_connection.main[count.index].id
-}
+  vpc_peering_connection_id  = aws_vpc_peering_connection.main.id
