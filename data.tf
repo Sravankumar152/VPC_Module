b18bb0e9f2bf0b9653a59612e911ab3929ed22f6
Sravankumar152 <thanatisravankumar756@outlook.com>
2026-03-04 14:17:47 +0530

    added awsroutemodule in data.tf

--- data.tf
+++ data.tf
@@ -3,3 +3,12 @@ data "aws_vpc" "default" {
 }
 
 data "aws_caller_identity" "current" {}
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
